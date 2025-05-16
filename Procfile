d32ab76b6c35bb3818142cbdb6c189933aa4ab8c
Om Mishra <ommishra@MacBookAir.lan>
2025-05-16 12:48:54 +0530

    Initial commit for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn audio-to-sign-converter.wsgi
---
+++ converter/static/converter/js/speech_v7.js
@@ -0,0 +1,67 @@
+ function startListening() {
+    const recognition = new (window.SpeechRecognition || window.webkitSpeechRecognition)();
+    recognition.lang = 'en-US';
+    recognition.start();
+
+    recognition.onresult = function(event) {
+        const spokenText = event.results[0][0].transcript.toLowerCase();
+        document.getElementById('output').innerText = `You said: ${spokenText}`;
+
+        const cleanedText = spokenText.replace(/[.,!?]/g, '');
+        const words = cleanedText.split(/\s+/);
+
+        const signDisplay = document.getElementById('sign-display');
+        signDisplay.innerHTML = '';
+
+        const knownPhrases = ["any questions", "are you angry", "are you busy", "are you hungry", "be careful", "did you finish homework", "do you have money", "do you want something to drink", "do you watch TV", "dont worry", "flower is beautiful", "good afternoon", "good morning", "good question", "i am a clerk", "i am fine", "i am sorry", "i am thinking", "i am tired", "i go to a theatre", "i had to say something but i forgot", "i like pink colour", "i love to shop", "lets go for lunch", "nice to meet you", "open the door", "please call me later", "please wait for sometime", "police station", "post office", "shall i help you", "shall we go together tomorrow", "sign language interpreter", "sit down", "stand up", "take care", "there was a traffic jam", "what are you doing", "what is the problem", "what is todays date", "what is your father do", "what is your mobile number", "what is your name", "whats up", "where is the bathroom", "where is the police station", "you are wrong"  ];
+
+        const knownWords = ["address", "ahemdabad", "all", "assam", "august", "banana", "banaras", "banglore", "bridge", "cat", "christmas", "church", "clinic", "dasara", "december", "grapes", "hello", "hindu", "hyderabad", "job", "july", "june", "karnataka", "kerala", "krishna", "mango", "may", "mile", "mumbai", "nagpur", "pakistan", "pune", "punjab", "saturday", "shop", "temple", "thursday", "toilet", "tomato", "tuesday", "usa", "village", "wednesday" ];
+
+        let i = 0;
+        while (i < words.length) {
+            let phraseMatched = false;
+
+            for (let phrase of knownPhrases) {
+                const phraseWords = phrase.split(' ');
+                const slice = words.slice(i, i + phraseWords.length).join(' ');
+
+                if (slice === phrase) {
+                    let img = document.createElement('img');
+                    img.src = `/static/converter/signs/${phrase}.gif`;
+                    img.alt = phrase;
+                    img.classList.add('sign-img');
+                    signDisplay.appendChild(img);
+                    i += phraseWords.length;
+                    phraseMatched = true;
+                    break;
+                }
+            }
+
+            if (!phraseMatched) {
+                const word = words[i];
+                if (knownWords.includes(word)) {
+                    let img = document.createElement('img');
+                    img.src = `/static/converter/signs/${word}.gif`;
+                    img.alt = word;
+                    img.classList.add('sign-img');
+                    signDisplay.appendChild(img);
+                } else {
+                    for (let char of word) {
+                        if (char.match(/[a-z]/)) {
+                            let img = document.createElement('img');
+                            img.src = `/static/converter/signs/letters/${char}.gif`;
+                            img.alt = char;
+                            img.classList.add('sign-img');
+                            signDisplay.appendChild(img);
+                        }
+                    }
+                }
+                i++;
+            }
+        }
+    };
+
+    recognition.onerror = function(event) {
+        console.error('Speech recognition error', event);
+    };
+}
